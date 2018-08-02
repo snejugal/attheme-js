39cfeb3e9639d8505fde543d2a1a4987279215ac
SnejUgal <snejugal@icloud.com>
2018-08-02 22:29:06 +0700

    Create some tools needed for compatibily with old APIs

---
+++ src/tools/themeToObject.ts
@@ -0,0 +1,35 @@
+import Attheme from "..";
+import { Color } from "../types";
+
+const IMAGE_KEY = Symbol.for(`image`);
+
+interface ObjectTheme {
+  [key: string]: Color;
+  [IMAGE_KEY]?: string;
+}
+
+/**
+ * Converts the new Attheme instance into the old one compatible with the old
+ * versions of attheme-js.
+ * @param theme The new Attheme instance.
+ * @returns An object compatible with the old versions of attheme-js.
+ */
+const themeToObject = (theme: Attheme): object => {
+  let object: ObjectTheme = {};
+
+  for (const [variable, value] of theme) {
+    if (variable === `__proto__`) {
+      continue;
+    }
+
+    object[variable] = value;
+  }
+
+  if (theme.hasWallpaper()) {
+    object[IMAGE_KEY] = theme.getWallpaper();
+  }
+
+  return object;
+};
+
+export default themeToObject;
