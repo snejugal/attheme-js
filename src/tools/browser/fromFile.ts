f33e54060cd073bacf28e7225cc4cb68a466e565
SnejUgal <snejugal@icloud.com>
2018-08-03 14:30:05 +0700

    Add a tool for easy reading of .attheme files in browsers

---
+++ src/tools/browser/fromFile.ts
@@ -0,0 +1,29 @@
+/// <reference lib="DOM" />
+
+import Attheme from "../..";
+import { AtthemeOptions } from "../../types";
+
+const fromFile = (
+  file: File,
+  options?: AtthemeOptions,
+): Promise<Attheme> => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    const chars = new Uint8Array(reader.result as ArrayBuffer);
+
+    let contents = ``;
+
+    for (const char of chars) {
+      contents += String.fromCharCode(char);
+    }
+
+    resolve(new Attheme(contents, options));
+  };
+
+  reader.onerror = () => reject(reader.error);
+
+  reader.readAsArrayBuffer(file);
+});
+
+export default fromFile;
