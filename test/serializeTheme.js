86f743485ccbfb0b16a42301813bc9f63a3bb6ec
SnejUgal <snejugal@icloud.com>
2018-08-04 12:34:02 +0700

    Write tests for `serializeTheme`

---
+++ test/serializeTheme.js
@@ -0,0 +1,38 @@
+import test from "ava";
+import serializeTheme from "../lib/serializeTheme";
+import Attheme from "../lib";
+
+test(`Serializes the theme correctly`, async (t) => {
+  const theme = new Attheme();
+
+  theme.setVariable(`divider`, {
+    red: 0xff,
+    green: 0xff,
+    blue: 0xff,
+    alpha: 0xff,
+  });
+
+  const wallpaper = `it's my life
+it's now or never
+i don't wanna live for ever
+i just wanna live while i'm alive`
+
+  theme.setWallpaper(wallpaper);
+
+  const expectedHexSerialization = `divider=#ffffffff
+
+WPS
+${wallpaper}
+WPE
+`;
+
+  const expectedIntSerialization = `divider=-1
+
+WPS
+${wallpaper}
+WPE
+`;
+
+  t.is(serializeTheme(theme, `hex`), expectedHexSerialization);
+  t.is(serializeTheme(theme, `int`), expectedIntSerialization);
+});
