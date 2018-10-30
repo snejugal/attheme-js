54a72bdacf1e9e5028508e1582f39ada8662ffc7
SnejUgal <snejugal@icloud.com>
2018-10-30 22:25:17 +0700

    Implement `toFile` function for Node.js

---
+++ src/tools/node/toFile.ts
@@ -0,0 +1,26 @@
+/// <reference types="node" />
+
+import * as fs from "fs";
+import Attheme from "../..";
+import { ColorSignature } from "../../types";
+
+/**
+ * Writes the theme into a file.
+ * @param theme The theme to write.
+ * @param path The file to write in.
+ * @param colorSignature Either `hex` or `int`.
+ * @returns Promised resolved when the file is written
+ */
+export default (
+  theme: Attheme,
+  path: string,
+  colorSignature?: ColorSignature,
+): Promise<void> => new Promise((resolve, reject) => {
+  fs.writeFile(path, theme.toString(colorSignature), `binary`, (error) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve();
+    }
+  });
+});
