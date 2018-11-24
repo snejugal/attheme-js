c2bd38c83cd09bd880ce0ec301a818cddd30d69c
SnejUgal <snejugal@icloud.com>
2018-11-24 15:41:36 +0700

    Write a script for generating maps

---
+++ scripts/generateThemeMap.js
@@ -0,0 +1,29 @@
+// Generates a map of the theme out of a typical .attheme file.
+//
+// Usage:
+//   node scripts/generateThemeMap < input.attheme > output.ts
+
+const Attheme = require(`..`).default;
+const themeContents = [];
+
+process.stdin.on(`data`, (chunk) => {
+  themeContents.push(...chunk);
+});
+
+process.stdin.on(`end`, () => {
+  const theme = new Attheme(Buffer.from(themeContents).toString(`binary`));
+
+  console.log(`import { Color } from "../types";`);
+  console.log(``);
+  console.log(`export default new Map<string, Color>([`);
+
+  for (const [variable, { red, green, blue, alpha }] of theme) {
+    console.log(`  [\`${variable}\`, {`);
+    console.log(`    red: ${red},`);
+    console.log(`    green: ${green},`);
+    console.log(`    blue: ${blue},`);
+    console.log(`    alpha: ${alpha},`);
+    console.log(`  }],`);
+  }
+  console.log(`]);`);
+});
