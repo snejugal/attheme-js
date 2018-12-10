85fb7c7139a0c1cc54538fc5d586646f87133b4c
SnejUgal <snejugal@icloud.com>
2018-12-10 23:28:01 +0700

    Revert missing variables

--- src/defaultThemes/default.ts
+++ src/defaultThemes/default.ts
@@ -1,6 +1,30 @@
 import { Color } from "../types";
 
 export default new Map<string, Color>([
+  [`chat_chats_menuTopShadow`, {
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 64,
+  }],
+  [`chat_serviceBackground`, {
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 102,
+  }],
+  [`chat_serviceBackgroundSelected`, {
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 136,
+  }],
+  [`chat_wallpaper`, {
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 255,
+  }],
   [`avatar_backgroundViolet`, {
     red: 142,
     green: 133,
