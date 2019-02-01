225e9c4bae7beb72ca3fdc6278ee37666b75f782
SnejUgal <snejugal@icloud.com>
2019-02-01 17:15:31 +0700

    Add flag `-d` that adds a few variables

--- scripts/generateThemeMap.js
+++ scripts/generateThemeMap.js
@@ -1,7 +1,13 @@
 // Generates a map of the theme out of a typical .attheme file.
 //
 // Usage:
-//   node scripts/generateThemeMap < input.attheme > output.ts
+//   node scripts/generateThemeMap [-d] < input.attheme > output.ts
+//
+// If the `-d` flag is passed, these variables are added to the theme:
+// - `chat_wallpaper`;
+// - `chat_serviceBackground`;
+// - `chat_servicebackgroundSelected`;
+// - `chats_menuTopShadow`.
 
 const Attheme = require(`..`).default;
 const themeContents = [];
@@ -12,6 +18,36 @@ process.stdin.on(`data`, (chunk) => {
 
 process.stdin.on(`end`, () => {
   const theme = new Attheme(Buffer.from(themeContents).toString(`binary`));
+
+  if (process.argv.includes(`-d`)) {
+    theme.set(`chat_wallpaper`, {
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 255,
+    });
+
+    theme.set(`chat_serviceBackground`, {
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 102,
+    });
+
+    theme.set(`chat_serviceBackgroundSelected`, {
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 136,
+    });
+
+    theme.set(`chats_menuTopShadow`, {
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 64,
+    });
+  }
 
   console.log(`import { Color } from "../types";`);
   console.log(``);
